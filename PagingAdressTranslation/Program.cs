b893dcbb65700720a35991c430429cb054967c8b
the-Dust <dust_@inbox.ru>
2017-07-25 13:20:37 +0400

    Translation logical to physical memory address in x86 paging long mode

---
+++ PagingAdressTranslation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace PagingAdressTranslation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file name");
+            string fileName = Console.ReadLine();
+            StreamReader sr = new StreamReader(fileName);
+            ulong[] temp = sr.ReadLine().Split().Select(ulong.Parse).ToArray();
+            int numberOfStrings = (int)temp[0];
+            int numberOfQueries = (int)temp[1];
+            ulong rootAddress = temp[2];
+            Dictionary<ulong, ulong> map = new Dictionary<ulong, ulong>();
+            ulong[] queries = new ulong[numberOfQueries];
+            string[] answers = new string[numberOfQueries];
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                ulong[] arr = sr.ReadLine().Split().Select(ulong.Parse).ToArray();
+                map.Add(arr[0], arr[1]);
+            }
+            Translator translator = new Translator(map, rootAddress);
+            for (int i = 0; i < numberOfQueries; i++)
+            {
+                queries[i] = ulong.Parse(sr.ReadLine());
+                answers[i] = translator.GetPhisycalAddress(queries[i]);
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter("answers.txt");
+            string answer = string.Join(Environment.NewLine, answers);
+            sw.Write(answer);
+            sw.Close();
+        }
+    }
+}
---
+++ PagingAdressTranslation/Translator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace PagingAdressTranslation
+{
+    class Translator
+    {
+        //page address - key, value in page address - value
+        Dictionary<ulong, ulong> map;
+        //address of the root table
+        ulong rootAddress;
+
+        public Translator(Dictionary<ulong, ulong> map, ulong rootAddress)
+        {
+            this.map = map;
+            this.rootAddress = rootAddress;
+        }
+
+        public string GetPhisycalAddress(ulong logicalAddress)
+        {
+            ulong mask12 = 4095; //"0000 1111 1111 1111"
+            ulong mask9 = 511; //"0000 0001 1111 1111"
+
+            ulong offset = logicalAddress & mask12;
+            logicalAddress >>= 12; //see the structure of long mode paging in x86
+
+            ulong table = logicalAddress & mask9;
+            logicalAddress >>= 9;
+
+            ulong directory = logicalAddress & mask9;
+            logicalAddress >>= 9;
+
+            ulong directoryPtr = logicalAddress & mask9;
+            logicalAddress >>= 9;
+
+            ulong PML4 = logicalAddress & mask9;
+
+            ulong[] numbers = { PML4, directoryPtr, directory, table};
+
+            ulong nextLevel = rootAddress;
+
+            for (int i = 0; i < 4; i++)
+            {
+                bool fail = false;
+                nextLevel = GetNextLevelAddress(nextLevel, numbers[i], ref fail);
+                if(fail)
+                    return "fault";
+            }
+
+            ulong address = nextLevel + offset;
+
+            return address.ToString();
+        }
+
+        private ulong GetNextLevelAddress(ulong level, ulong number, ref bool fail)
+        {
+            //we take the address of table (level) and adding number of note * 8 (size of each note in bytes)
+            ulong key = level + 8 * number;
+            //if the map doesn't contain a key, we take value = 0 (according to condition of the task)
+            ulong nextLevel = map.ContainsKey(key) ? map[key] : 0;
+            //checking a "p"-bit (see the structure of the note in long mode table in x86)
+            //if p==0 the note isn't in use
+            if ((nextLevel & 1) == 0)
+                fail = true;
+            //making align to 12 younger bits
+            else
+            {
+                nextLevel >>= 12;
+                nextLevel <<= 12;
+            }
+            return nextLevel;
+        }
+    }
+}
